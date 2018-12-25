045caff5068c54895c0d3aa3be9648a89c32f684
Wenqin <wenqinYe@users.noreply.github.com>
2018-12-25 18:32:16 -0500

    merge fix

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,5 +3,7 @@
 class HomepageController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def homepage; end
+  def homepage
+    @events = Event.where('end_time >= ?', DateTime.now)
+  end
 end
