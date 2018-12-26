95b728b8f42d58e55014c5dac63f2da417b175f9
Wenqin <wenqinYe@users.noreply.github.com>
2018-12-25 21:01:11 -0500

    Homepage redesign (#26)
    Redesigned the homepage :D

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,5 +3,7 @@
 class HomepageController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def homepage; end
+  def homepage
+    @events = Event.where('end_time >= ? AND approved=true', Time.now).order('start_time ASC')
+  end
 end
--- test/controllers/homepage_controller_test.rb
+++ test/controllers/homepage_controller_test.rb
@@ -3,8 +3,23 @@
 require 'test_helper'
 
 class HomepageControllerTest < ActionDispatch::IntegrationTest
-  test 'home page returns a ok response' do
+  setup do
+    @approved_lecture = events(:approved_lecture)
+    @non_approved_lecture = events(:non_approved_lecture)
+    @admin_lecture = events(:admin_lecture)
+    @past_lecture = events(:past_lecture)
+  end
+
+  test '#homepage returns an ok response' do
     get root_path
     assert_response :ok
+  end
+
+  test '#homepage lists upcoming events' do
+    get root_path
+
+    assert_includes assigns(:events), @approved_lecture
+    refute_includes assigns(:events), @non_approved_lecture
+    refute_includes assigns(:events), @past_lecture
   end
 end
