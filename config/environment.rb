a151958d799e3a122a7b7a29dc1c3f8a3f929131
Peter Zhu <peter@peterzhu.ca>
2018-12-26 22:32:52 -0500

    Add styling to devise views (#30)
    * Add styling to devise views
    
    * Make rubocop happy

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionView::Base.field_error_proc = proc do |html_tag, _instance|
+  html_tag.html_safe
+end
